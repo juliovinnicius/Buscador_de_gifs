e65a21b39363c6215a29bc97f201c4c73f23d1b0
juliovinnicius <julio.macena94@gmail.com>
2019-10-22 18:58:56 -0300

    Primeiro Commit

---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/ui/gif_page.dart';
+import 'ui/home_page.dart';
+
+void main(){
+
+  runApp( MaterialApp(
+    title: "Giphy",
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ) );
+}
+
